127ff05617b98252a6218e9c2ed4403f32c9217a
FatimaMelo <fatimabsmelo@gmail.com>
2022-10-24 11:55:01 -0300

    trabalhos2410parte1

---
+++ ProjetoFatima/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoFatima.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("nome")]
+        [Display(Name = "Produto")]
+        public string Nome { get; set; }
+
+        [Column("preco")]
+        [Display(Name = "Preço")]
+        public double Preco { get; set; }
+
+
+
+
+    }
+}
